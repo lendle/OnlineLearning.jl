1ef82199dcc65d0f098dfb06bd973a5984bff3d6
Sam Lendle <sam.lendle@gmail.com>
2014-06-11 01:12:35 -0700

    Remove projectsimplex

--- src/extra.jl
+++ src/extra.jl
@@ -10,19 +10,7 @@ evaluate(::Qgrad, r, tau) = r == zero(r)? zero(r) :
                                  r < zero(r)? oftype(r, -tau):
                                  oftype(r, 1.0-tau)
 
-
 emptyvector{T}(::Type{T}) = Array(T, 0)
-
-
-function projectsimplex{T <: Real}(v::Array{T, 1}, z::T=one(T))
-  n=length(v)
-  µ = sort(v, rev=true)
-  #finding ρ could be improved to avoid so much temp memory allocation
-  ρ = maximum((1:n)[µ - (cumsum(µ) .- z) ./ (1:n) .>0])
-  θ = (sum(µ[1:ρ]) - z)/ρ
-  max(v .- θ, 0)
-end
-
 
 Base.At_mul_B!{T<:Base.LinAlg.BlasFloat}(α::Number,A::StridedMatrix{T},x::StridedVector{T},β::Number,y::StridedVector{T}) =
   BLAS.gemv!('T', oftype(T, α), A, x, oftype(T, β), y)
